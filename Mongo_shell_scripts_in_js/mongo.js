4349d32a4817510ee818eb41960e7f9b6e1d6bac
Pratham1128 <prathameshmadane45@gmail.com>
2025-11-14 23:55:00 +0530

    learned mongo playground

--- Mongo_shell_scripts_in_js/mongo.js
+++ Mongo_shell_scripts_in_js/mongo.js
@@ -1,6 +1,10 @@
-    console.log("Hiii");
-    console.log(db);
-    console.log(show);
-    console.log(use);
-    
-    
+use("hostelDB")
+// db.users.insertOne({name:"Prathmaesh"})
+//  console.log(db.users.find());
+//  db.users.insertOne({taskCOmpleted:true})
+
+//can insert on loop 
+const userCollection=db.getCollection("user")
+for(let i=0;i<10000;i++){
+    userCollection.insertOne({name:`$user${i}`,password:`${(Math.random()*1000000)+1}`})
+}
